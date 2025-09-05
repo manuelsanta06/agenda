918536048ee11508828ea4576514967987f22665
santa <manuelsanta06@gmail.com>
2025-09-05 09:15:51 -0300

    basic toolbar

---
+++ lib/pages/calendar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class calendarPage extends StatelessWidget {
+  const calendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/pages/people.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class peoplePage extends StatelessWidget {
+  const peoplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
